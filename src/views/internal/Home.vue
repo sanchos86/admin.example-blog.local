a54771b9a6812eb16a03f97c6941980924f75b39
Aleksandr Aladin <aleksandr_aladin@outlook.com>
2021-02-27 14:01:34 +0300

    update home view

--- src/views/internal/Home.vue
+++ src/views/internal/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home">
-    Home
-  </div>
+  <v-container class="py-0" fluid>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-h4">Главная</h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
